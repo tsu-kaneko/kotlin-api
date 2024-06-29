82c0f064e02d23d479f352c271f5f2258ef47552
tsu-kaneko <tsu.kaneko0410@gmail.com>
2024-06-29 17:19:31 +0900

    init

---
+++ src/main/java/com/example/kotlinapi/KotlinApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotlinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/kotlinapi/SampleController.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController {
+    @GetMapping("/")
+    fun getHello() :String {
+        return "Hello Kotlin"
+    }
+}
