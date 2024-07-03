eb99a7d599817db9b9145249cad8aebf4e6d7f7f
tsu-kaneko <tsu.kaneko0410@gmail.com>
2024-07-03 12:01:59 +0900

    class name fix

--- src/main/java/com/example/kotlinapi/HelloController.kt
+++ src/main/java/com/example/kotlinapi/HelloController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SampleController {
+class HelloController {
     @GetMapping("/")
     fun getHello() :String {
         return "Hello Kotlin!!!"
